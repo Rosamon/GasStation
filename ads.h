e73997c35bce72355221029a55a9eaa33a7d6196
NDK619 <KuvshinovND@yandex.ru>
2021-12-13 15:07:33 +0300

    Update ads.h

--- ads.h
+++ ads.h
@@ -3,10 +3,11 @@
 //Запуск главного меню
 #include "clases.h"
 int menu();
-int MyMainMenu(AutoList);
+int MyMainMenu(AutoList, PartList)
 int SaveDataAuto(int, AutoList&);
 int WorkAutoScreen(AutoList&);
 int WorkPartsScreen(PartList&);
 int WorkReportScreen(AutoList&, PartList&);
 int writeAutoReport(AutoList*);
 int writePartsReport(PartList*);
+int WorkPartsScreen(PartList&);
